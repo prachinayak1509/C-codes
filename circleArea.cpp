4ccb17bc0568f6e0df2f504bcf8f365fa31478d4
prachinayak1509 <prachinayak1509@gmail.com>
2022-09-16 21:54:10 +0530

    first commit

---
+++ circleArea.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int radius;
+    float area;
+    cout << "enter radius";
+    cin >> radius;
+    area=3.14*radius*radius;
+    cout << "area of circle:" << area;
+    return 0;
+}
---
+++ helloWorld.cpp
@@ -0,0 +1,9 @@
+// Your First C++ Program
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello Prachi Nayak!";
+    return 0;
+}
