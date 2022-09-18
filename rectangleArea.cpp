6bdbcee6c858298fc5d4ab22e1119c9b28b3bfbd
prachinayak1509 <prachinayak1509@gmail.com>
2022-09-18 22:35:21 +0530

    added

---
+++ rectangleArea.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int length,breadth,area;
+    cout << "enter length:";
+    cin >> length;
+    cout << "enter breath:";
+    cin >> breadth;
+    area=length*breadth;
+    cout << "area of rectangle area:" << area;
+    return 0;
+}
